66a3adb156975896597172c2dd2fdaba887f4d3d
adamuremek <adamuremek@gmail.com>
2024-03-18 15:29:52 -0400

    Initial Commit

---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef REGOLITH_LIBRARY_H
+#define REGOLITH_LIBRARY_H
+
+void hello();
+
+#endif //REGOLITH_LIBRARY_H
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <enet/enet.h>
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+}
