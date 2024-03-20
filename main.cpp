a56e6987d6169ede5e571d9e9e809a83706630e2
adamuremek <adamuremek@gmail.com>
2024-03-20 15:21:03 -0400

    Playing around with ideas.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
 #include <enet/enet.h>
+#include <vector>
+#include <cstdio>
+
+struct Message{
+    uint8_t* p_data = nullptr;
+    size_t p_size = 0;
+    void release(){
+        delete p_data;
+        p_data = nullptr;
+        p_size = 0;
+    }
+};
+
+class Serializer{
+
+};
+
+class Deserializer{
+
+};
+
+template<typename T>
+void serialize(T in){
+    std::cout << "IS GENERIC" << std::endl;
+}
+
+#define SERIALIZE template<> void serialize
+
+template<typename T>
+Message createMessage(const T* data, size_t size){
+    const uint8_t* byteData = reinterpret_cast<const uint8_t*>(data);
+
+    Message mssg;
+    mssg.p_data = new uint8_t[size];
+    mssg.p_size = size;
+
+    std::memcpy(mssg.p_data, byteData, size);
+
+    return mssg;
+}
+
+//SERIALIZE<int>(int in){
+//    std::cout << "IS INT" << std::endl;
+//}
+//
+SERIALIZE<float>(float in){
+    std::cout << "IS FLOAT" << std::endl;
+}
+
 
 int main(){
-    std::cout << "Hello World!" << std::endl;
+    int a = 5;
+    float b = 2.0f;
+
+    serialize(a);
+    serialize(b);
+
 }
+
+
+
+
