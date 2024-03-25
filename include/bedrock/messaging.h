9b6a66532f78d074d8287c9f3d077ef25acdfaee
adamuremek <adamuremek@gmail.com>
2024-03-25 13:12:32 -0400

    Added some structure.

---
+++ include/bedrock/messaging.h
@@ -0,0 +1,40 @@
+#ifndef BEDROCK_MESSAGING_H
+#define BEDROCK_MESSAGING_H
+
+class MessageCallbackRegistry{
+private:
+    std::unordered_map<uint32_t,  std::function<void(ByteStream)>> callbacks;
+public:
+    static MessageCallbackRegistry& getInstance(){
+        static MessageCallbackRegistry callbackRegistry;
+        return callbackRegistry;
+    }
+
+    template<typename T>
+    void registerCallback(std::function<void(T)> callback){
+        if(!std::is_base_of<BedrockDataType<T>, T>::value){
+            return; // Ensure T is derived from BedrockDataType
+        }
+
+        callbacks[T::dataId] = [callback](ByteStream stream){
+            T t{};
+
+            std::memcpy(&t, stream + sizeof(uint32_t ), sizeof(T));
+
+            callback(t);
+
+        };
+    }
+
+    void executeCallback(ByteStream stream){
+        uint32_t id = 0;
+        std::memcpy(&id, stream, sizeof(uint32_t));
+
+        if(callbacks.find(id) != callbacks.end()){
+            callbacks[id](stream);
+        }
+    }
+};
+
+
+#endif //BEDROCK_MESSAGING_H
